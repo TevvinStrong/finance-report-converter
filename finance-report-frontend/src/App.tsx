6b0c7a17ecf2b26a0fcddcb3122bf9f80e5ff7ac
TevvinStrong <tevvindstrong@gmail.com>
2025-02-22 23:19:44 -0500

    FRPV-0001: Frontend done for now, needs backend

---
+++ finance-report-frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+// Components
+import UploadPage from "./components/UploadPage";
+
+function App() {
+  return (
+    <div className="App">
+      <UploadPage />
+    </div>
+  );
+}
+
+export default App;
